938c716f14d83e36d59d5418925489b908c2b4b2
Bab95 <babnish95@gmail.com>
2021-05-03 03:53:29 +0530

    Added Sql Script and Updated Dependencies
    Added Sql Script and Updated Dependencies
    
    Co-Authored-By: Captain_Levi_Ackerman <babnish95@gmail.com>

--- src/main/java/com/shopping/springbootecommerce/entity/ProductCategory.java
+++ src/main/java/com/shopping/springbootecommerce/entity/ProductCategory.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name="product_category")
+@Table(name="product_category", schema = "full-stack-ecommerce")
 @Getter
 @Setter
 public class ProductCategory {
