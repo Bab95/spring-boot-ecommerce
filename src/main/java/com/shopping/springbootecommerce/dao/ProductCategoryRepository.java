5b08e4f913b8bef91f36e217319eaf39baba4cc1
Babnish Vyas <babnishvyas@microsoft.com>
2021-05-03 01:41:36 +0530

    Adding Repository Files and Fixing application.properties

---
+++ src/main/java/com/shopping/springbootecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.shopping.springbootecommerce.dao;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository <ProductCategory, Long>{
+}
