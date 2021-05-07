617e214c1b90920c873dfdb315da0b189d255b50
Bab95 <babnish95@gmail.com>
2021-05-07 21:02:53 +0530

    Removing CrossOriginError in FrontEnd

--- src/main/java/com/shopping/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/com/shopping/springbootecommerce/dao/ProductRepository.java
@@ -3,7 +3,9 @@ package com.shopping.springbootecommerce.dao;
 import com.shopping.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(collectionResourceRel = "Product", path = "product")
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "products", path = "product")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
