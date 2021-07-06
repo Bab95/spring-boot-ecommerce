4d757d233019ba2a27d6ec020312447b2f5630d0
Babnish Vyas <babnishvyas@microsoft.com>
2021-07-06 23:30:46 +0530

    commit-2

--- src/main/java/com/shopping/springbootecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/shopping/springbootecommerce/config/MyDataRestConfig.java
@@ -26,5 +26,6 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unSupportedActions)));
         // Commment to verify the GIT checkout of commit and changing.......
         //commit 1
+        //commit 2
     }
 }
