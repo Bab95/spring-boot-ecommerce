5905ec51b0866342a45c0a3b56dc6da99899908f
Babnish Vyas <babnishvyas@microsoft.com>
2021-07-06 23:52:15 +0530

    testing single file checkout from a commit

--- src/main/java/com/shopping/springbootecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/shopping/springbootecommerce/dao/ProductCategoryRepository.java
@@ -1,10 +1,8 @@
 package com.shopping.springbootecommerce.dao;
 
-import com.shopping.springbootecommerce.entity.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+public interface ProductCategoryRepository <ProductCategory, Long>{
+    // Nothing just testing something....
 }
