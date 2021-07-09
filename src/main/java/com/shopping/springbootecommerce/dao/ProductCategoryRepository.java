867ae54fcccf93c48e19bddef88f098f660f3a6f
Babnish Vyas <babnishvyas@microsoft.com>
2021-07-09 15:13:05 +0530

    git checkout file check

--- src/main/java/com/shopping/springbootecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/shopping/springbootecommerce/dao/ProductCategoryRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository <ProductCategory, Long>{
     // Nothing just testing something....
-    //changin this file in dev branch
-    //test commit 2
 }
